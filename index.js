ea231ee14028b31a07f4503d9932dd3d97f09041
Your UsernameLatifa <elazzaou.latifa@gmail.com>
2024-06-07 14:28:32 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,119 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const users = require("./routes/users");
+const postsRoute = require("./routes/posts");
+
+const error = require("./utilities/error");
+
+const app = express();
+const port = 3000;
+// storge for posts
+let post=[
+  { id: 1, userId: 1, title: 'First Post', content: 'This is the first post.' },
+  { id: 2, userId: 2, title: 'Second Post', content: 'This is the second post.' },
+  { id: 3, userId: 1, title: 'Another Post', content: 'This is another post by user 1.' }
+];
+// Export the posts array
+exports.posts = posts;
+
+// Parsing Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ extended: true }));
+
+// Logging Middlewaare
+app.use((req, res, next) => {
+  const time = new Date();
+
+  console.log(
+    `-----
+${time.toLocaleTimeString()}: Received a ${req.method} request to ${req.url}.`
+  );
+  if (Object.keys(req.body).length > 0) {
+    console.log("Containing the data:");
+    console.log(`${JSON.stringify(req.body)}`);
+  }
+  next();
+});
+
+// Valid API Keys.
+const apiKeys = ["perscholas", "ps-example", "hJAsknw-L198sAJD-l3kasx"];
+
+// New middleware to check for API keys!
+// Note that if the key is not verified,
+// we do not call next(); this is the end.
+// This is why we attached the /api/ prefix
+// to our routing at the beginning!
+app.use("/api", function (req, res, next) {
+  var key = req.query["api-key"];
+
+  // Check for the absence of a key.
+  if (!key) next(error(400, "API Key Required"));
+
+  // Check for key validity.
+  if (apiKeys.indexOf(key) === -1) next(error(401, "Invalid API Key"));
+
+  // Valid key! Store it in req.key for route access.
+  req.key = key;
+  next();
+});
+
+// Use our Routes
+app.use("/api/users", users);
+app.use("/api/posts", postspostsRoute);
+
+// Adding some HATEOAS links.
+app.get("/", (req, res) => {
+  res.json({
+    links: [
+      {
+        href: "/api",
+        rel: "api",
+        type: "GET",
+      },
+    ],
+  });
+});
+
+// Adding some HATEOAS links.
+app.get("/api", (req, res) => {
+  res.json({
+    links: [
+      {
+        href: "api/users",
+        rel: "users",
+        type: "GET",
+      },
+      {
+        href: "api/users",
+        rel: "users",
+        type: "POST",
+      },
+      {
+        href: "api/posts",
+        rel: "posts",
+        type: "GET",
+      },
+      {
+        href: "api/posts",
+        rel: "posts",
+        type: "POST",
+      },
+    ],
+  });
+});
+
+// 404 Middleware
+app.use((req, res, next) => {
+  next(error(404, "Resource Not Found"));
+});
+
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({ error: err.message });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port: ${port}.`);
+});
